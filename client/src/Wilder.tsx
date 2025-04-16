1ea68a9eea0b3dc6ce549ff6fe3a4f0ff242835c
CarolineGensac <gensac.caroline@gmail.com>
2025-04-16 12:15:15 +0200

    MEMEMEEH

--- client/src/Wilder.tsx
+++ client/src/Wilder.tsx
@@ -14,7 +14,7 @@ function Wilder({ city, name }: WilderProps) {
       <h3>{name}</h3>
       <h4>City</h4>
       <p>{city}</p>
-      <p> oula blabla BLABLA BLABAKMFDPIGHNFKGNbddgbg JHDGFHJKHJ?NDVSFGHJKH?NBVCX MEEEEEH</p>
+      <p> oula blabla BLABLA BLABAKMFDPIGHNFKGNbddgbg JHDGFHJKHJ?NDVSFGHJKH?NBVCX MEMEMEMEM MEEEEEH</p>
     </Card>
   );
 }
